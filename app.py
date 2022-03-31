0e9872c408a0380b18006dbc7e2d646d0b1ed442
ronituohino <tuohinoroni@gmail.com>
2022-03-31 18:21:13 +0300

    Add: Site icon

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, send_file, request, redirect
 # Pylance suppressed warning for this import
 from flask_sqlalchemy import SQLAlchemy  # type: ignore
 from os import getenv
@@ -24,3 +24,28 @@ def restaurant(id):
     result = db.session.execute(sql, {"id": id})
     restaurant = result.fetchone()
     return render_template("restaurant.html", restaurant=restaurant)
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_file("static/icon/favicon.ico", mimetype="image/x-icon")
+
+
+@app.route("/apple-touch-icon.png")
+def apple_favicon():
+    return send_file("static/icon/apple-touch-icon.png", mimetype="image/png")
+
+
+@app.route("/favicon-32x32.png")
+def thirtytwo_favicon():
+    return send_file("static/icon/favicon-32x32.png", mimetype="image/png")
+
+
+@app.route("/favicon-16x16.png")
+def sixteen_favicon():
+    return send_file("static/icon/favicon-16x16.png", mimetype="image/png")
+
+
+@app.route("/site.webmanifest")
+def manifest():
+    return send_file("static/icon/site.webmanifest")
